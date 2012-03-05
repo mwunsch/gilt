bdab275e9d96b84535b1df6ff7e0a91ac83117f7
Mark Wunsch <mark@markwunsch.com>
2012-03-05 13:37:58 -0500

    Client::Products specs

--- spec/gilt/client/products_spec.rb
+++ spec/gilt/client/products_spec.rb
@@ -1,6 +1,33 @@
 require 'spec_helper'
 
 describe Gilt::Client::Products do
+  before do
+    @apikey = "my-api-key"
+    @affid = "my-affiliate-id"
+    @client = described_class.new @apikey, @affid
+    stub_request :any, /api\.gilt\.com\/.*/
+  end
+
+  describe "::domain" do
+    it "points to the api.gilt.com domain" do
+      described_class.domain.should match "api.gilt.com/v1"
+    end
+  end
+
   describe "#initialize" do
+    it "inherits from Weary::Client" do
+      @client.should be_kind_of Weary::Client
+    end
+  end
+
+  describe "#detail" do
+    it "generates a request to get the details for a product" do
+      detail = @client.detail :product_id => "a-product-id"
+      detail.uri.to_s.should match "/products/a-product-id/detail"
+    end
+
+    it "raises an error when no product id is given" do
+      expect { @client.detail }.to raise_error Weary::Resource::UnmetRequirementsError
+    end
   end
 end

f566f682e3ed03d3497da0d02a5e89865d29da0f
Mark Wunsch <mark@markwunsch.com>
2012-03-07 13:51:11 -0500

    1.8.7 block compatability

--- lib/gilt/client/sales.rb
+++ lib/gilt/client/sales.rb
@@ -23,12 +23,14 @@ module Gilt
         Gilt::Stores::MEN,
         Gilt::Stores::KIDS,
         Gilt::Stores::HOME ].each do |store|
-        define_method "active_in_#{store}" do |params={}, &block|
+        define_method "active_in_#{store}" do |*args, &block|
+          params = args.first || {}
           active_in_store params.merge({:store => store}), &block
         end
         alias_method "#{store}_active".intern, "active_in_#{store}".intern
 
-        define_method "upcoming_in_#{store}" do |params={}, &block|
+        define_method "upcoming_in_#{store}" do |*args, &block|
+          params = args.first || {}
           upcoming_in_store params.merge({:store => store}), &block
         end
         alias_method "#{store}_upcoming".intern, "upcoming_in_#{store}".intern

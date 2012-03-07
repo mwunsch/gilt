35407664ecca4d6e857339ba5b838ddbc8bfa342
Mark Wunsch <mark@markwunsch.com>
2012-03-06 23:18:42 -0500

    Sale model beginning

--- lib/gilt/sale.rb
+++ lib/gilt/sale.rb
@@ -1,7 +1,85 @@
+require 'time'
+
 module Gilt
   class Sale
     def self.client(apikey, affid=nil)
       Gilt::Client::Sales.new(apikey, affid)
+    end
+
+    def self.product_client(apikey, affid=nil)
+      Gilt::Client::Products.new(apikey, affid)
+    end
+
+    def self.detail(store, sale_key, apikey, affid=nil)
+      client(apikey, affid).detail :store => store,
+                                   :sale_key => sale_key
+    end
+
+    def self.create(store, sale_key, apikey, affid=nil)
+      response = detail(store, sale_key, apikey, affid).perform
+      self.new response.parse, product_client(apikey, affid)
+    end
+
+    def initialize(sale_body, client=nil)
+      @sale = sale_body
+      @client = client
+    end
+
+    def name
+      @sale["name"]
+    end
+
+    def store
+      @sale["store"].intern
+    end
+
+    def sale_key
+      @sale["sale_key"]
+    end
+
+    def sale
+      URI(@sale["sale"])
+    end
+
+    def sale_url
+      URI(@sale["sale_url"])
+    end
+
+    def images
+      @sale["image_urls"]
+    end
+
+    def description
+      @sale["description"]
+    end
+
+    def begins
+      Time.parse @sale["begins"]
+    end
+
+    def ends
+      end_time = @sale["ends"]
+      Time.parse end_time unless end_time.nil?
+    end
+
+    def ended?
+      return false if ends.nil?
+      ends < Time.now
+    end
+
+    def duration
+      (ends - begins).ceil unless ends.nil?
+    end
+
+    def products
+      return @products unless @products.nil?
+      resource = Gilt::Client::Products.resources[:detail]
+      @products = @sale["products"].map do |product|
+        id = resource.url.extract(product)["product_id"]
+        @client.detail(:product_id => id)
+        # TODO: Perform request, but defer product creation until complete
+        # Likely, create a DeferredProduct class
+      end
     end
 
   end
---
+++ spec/gilt/sale_spec.rb
@@ -0,0 +1,132 @@
+require 'spec_helper'
+
+describe Gilt::Sale do
+  before do
+    @apikey = "my-api-key"
+    @affid = "my-affiliate-id"
+    @sale_key = "shoshanna-019"
+    stub_request(:any, /api\.gilt\.com\/.+\/detail\.json/).to_return fixture('sale_detail.json')
+  end
+
+  describe "::client" do
+    it "creates a new instance of the Sales client" do
+      described_class.client(@apikey, @affid).should be_kind_of Gilt::Client::Sales
+    end
+  end
+
+  describe "::detail" do
+    it "creates an instance of Sale from the sale response" do
+      detail = described_class.detail(Gilt::Stores::WOMEN, @sale_key, @apikey)
+      detail.should be_kind_of Weary::Request
+    end
+  end
+
+  describe "::create" do
+    it "creates an instance of Sale from the request response" do
+      sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+      sale.should be_instance_of described_class
+    end
+  end
+
+  describe "#name" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "returns the sale's name" do
+      @sale.name.should match /Shoshanna/
+    end
+  end
+
+  describe "#store" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "returns the sale's store" do
+      @sale.store.should eq Gilt::Stores::WOMEN
+    end
+  end
+
+  describe "#sale_key" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "returns the sale's sale key" do
+      @sale.sale_key.should match /^shoshanna/
+    end
+  end
+
+  describe "#sale" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "returns a URI representation of the sale's endpoint" do
+      @sale.sale.to_s.should match "/sales/#{Gilt::Stores::WOMEN}/#{@sale.sale_key}/"
+    end
+  end
+
+  describe "#sale_url" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "returns a URI representation of the sale page" do
+      @sale.sale_url.to_s.should match "www.gilt.com/sale/women/#{@sale.sale_key}"
+    end
+  end
+
+  describe "#begins" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "returns a Time representation of the sale begin time" do
+      @sale.begins.day.should eql 5
+    end
+  end
+
+  describe "#ends" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "returns a Time representation of the sale end time" do
+      @sale.ends.day.should eql 7
+    end
+  end
+
+  describe "#ended?" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "is true unless you have traveled back in time" do
+      @sale.ended?.should be_true
+    end
+  end
+
+  describe "#duration" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "returns the number of seconds between begin and end" do
+      (@sale.duration / 60 / 60).should eql 36
+    end
+  end
+
+  describe "#products" do
+    before do
+      @sale = described_class.create(Gilt::Stores::WOMEN, @sale_key, @apikey)
+    end
+
+    it "returns a map of product requests" do
+      @sale.products.should be_all {|p| p.is_a? Weary::Request }
+    end
+
+  end
+
+end
